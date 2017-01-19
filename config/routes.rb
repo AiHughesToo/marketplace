2766818c9b804fd0bb34fd8737d0d7e1e1df9deb
AiHughesToo <alex.hughes@interapthq.com>
2017-01-19 13:22:43 -0500

    sets the routes constraints for the api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 MarketPlaceApi::Application.routes.draw do
-
+  # Api deffinition
+  namespace :api, defaults: { format: :json },
+                      constraints: { subdomain: 'api' }, path: '/' do
+    # list resources here
+  end
 end
